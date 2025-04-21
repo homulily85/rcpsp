56988fc540183aefaf316d1276bfc3b310c8422a
homulily85 <nguyenkimtrungduc2005@gmail.com>
2025-04-22 02:56:18 +0700

    Add bash file for running on VPS

---
+++ bound/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if encoder_type argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <encoder_type>"
+  exit 1
+fi
+
+encoder_type=$1
+
+# List of datasets
+datasets=("j30.sm" "j60.sm" "j90.sm" "j120.sm" "pack" "pack_d")
+
+# Loop through datasets and run the benchmark
+for data_set in "${datasets[@]}"; do
+  echo "Running benchmark for dataset: $data_set with encoder: $encoder_type"
+  python benchmark.py "$data_set" "$encoder_type" 1800 --verbose --verify --show_solution
+done
---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install -y build-essential gdb
+sudo apt install -y python3.12-dev python3.12-venv
+python3 -m venv ./
+source ./bin/activate
+pip install -r requirements.txt
+chmod ./tt-open-wbo-inc-Glucose4_1_static

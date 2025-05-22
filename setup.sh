1b0e1925b7932cc4a357926c9c78f71c3bab271d
homulily85 <nguyenkimtrungduc2005@gmail.com>
2025-05-23 01:44:28 +0700

    Small change in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 sudo apt-get update
 sudo apt-get install -y build-essential gdb
 sudo apt install -y python3.12-dev python3.12-venv
-python3 -m venv ./
-source ./bin/activate
+python3 -m venv ./.venv
+source ./.venv/bin/activate
 pip install -r requirements.txt
 chmod +x ./bin/tt-open-wbo-inc-Glucose4_1_static
 chmod +x ./bin/mrcpsp2smt
